289033c5606160accdc4a1f4b657dd429abd505e
George Needham <34216187+D3ord3NidAm@users.noreply.github.com>
2022-07-24 04:48:40 -0500

    Update config.js
    V14 has required "activities" to be in an Array[ ]. The type can only be used if you use an array, otherwise it will default to "Playing".

--- config.js
+++ config.js
@@ -3,10 +3,11 @@ module.exports = {
     default_prefix: '!',
     owners: ['Your ID'],
     presence: {
-        activities: { 
+        activities: [{ 
             name: 'you',
             type: 3, // https://stackoverflow.com/questions/73049373/setpresence-activity-type-in-discord-js-v14-can-only-be-set-to-playing/73050109
-        },
+            //type can be anything 0-5; 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom, 5 = Competing
+        }],
         status: 'dnd',
     },
 };
